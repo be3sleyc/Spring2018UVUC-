648960decd2b89ff6a59b3d486706aa875131b6a
Christian <crmbeesley@gmail.com>
2018-03-17 16:43:27 -0600

    Added timer form and updated comments

---
+++ CBMyGradeApp/QuizTimerForm.cs
@@ -0,0 +1,31 @@
+﻿//Name: Christian Beesley
+//Class: INFO 1200
+//Section: 001
+//Professor: Crandall
+//Date: 20180317
+//Project #: 6A
+//I declare that the source code contained in this assignment was written solely by me.
+//I understand that copying any source code, in whole or in part,
+// constitutes cheating, and that I will receive a zero on this project
+// if I am found in violation of this policy.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CBMyGradeApp
+{
+    public partial class QuizTimerForm : Form
+    {
+        public QuizTimerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
